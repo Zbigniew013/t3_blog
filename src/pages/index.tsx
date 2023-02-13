2da57fe83b778ecc42376eb25769e4c08ff1118d
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-02-13 10:50:09 +0100

    building main section - posts section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ const HomePage = () => {
         </div>
       </header>
       <section className="grid h-full w-full grid-cols-12 place-items-center">
-        <main className="col-span-8 h-full w-full border-r border-gray-300 ">
-          <div className="flex w-full flex-col space-y-4 py-10 px-24">
+        <main className="col-span-8 h-full w-full border-r border-gray-300 px-24 ">
+          <div className="flex w-full flex-col space-y-4 py-10 ">
             <div className="flex w-full items-center space-x-4 ">
               <label
                 htmlFor="search"
@@ -76,7 +76,60 @@ const HomePage = () => {
             </div>
           </div>
 
-          <div>main part</div>
+          <div className="flex w-full flex-col justify-center space-y-8 ">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <div
+                key={i}
+                className="group flex flex-col space-y-4 border-b border-gray-300 pb-8 last:border-none "
+              >
+                <div className="flex w-full items-center space-x-2 ">
+                  <div className="h-10 w-10  rounded-full bg-gray-400 "></div>
+                  <div>
+                    <p className="font-semibold">
+                      Zbigniew Szałkowski &#x2022; 22 Dec. 2022
+                    </p>
+                    <p className="text-sm">Father & developer</p>
+                  </div>
+                </div>
+                <div className="grid w-full grid-cols-12 gap-4 ">
+                  <div className="col-span-8 flex flex-col space-y-4">
+                    <p className="text-2xl font-bold text-gray-800 decoration-indigo-600 group-hover:underline ">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Ab sequi dolorem!
+                    </p>
+                    <p className="break-words text-sm text-gray-500 ">
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Odit recusandae numquam quia repellendus, reprehenderit
+                      consectetur aut quas ab perferendis error suscipit facere.
+                      Repellendus voluptatum labore at illo magni et
+                      perspiciatis eligendi inventore nemo aliquid magnam soluta
+                      reprehenderit, explicabo odit molestiae ipsam delectus
+                      velit fugiat reiciendis voluptatem debitis voluptates
+                      dicta sapiente. Consectetur ratione magnam quasi facere
+                      illo odit excepturi, repellendus doloribus.
+                    </p>
+                  </div>
+                  <div className="col-span-4">
+                    <div className="h-full w-full transform rounded-xl bg-gray-300 transition duration-300 hover:scale-105 hover:shadow-xl "></div>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex w-full items-center justify-start space-x-4 ">
+                    <div className="flex items-center space-x-2 ">
+                      {Array.from({ length: 4 }).map((_, i) => (
+                        <div
+                          key={i}
+                          className="rounded-2xl bg-gray-200/50 px-5 py-3  "
+                        >
+                          tag {i}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </main>
         <aside className="col-span-4 h-full w-full ">
           this is used for side bar
