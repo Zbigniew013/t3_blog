52d7f2a68dd6a611884219facbf22cf1381cb3d5
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-02-14 13:45:03 +0100

    building sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { HiChevronDown } from "react-icons/hi";
 
 const HomePage = () => {
   return (
-    <div className="flex h-screen w-full flex-col ">
+    <div className="flex h-full w-full flex-col ">
       <header className="flex h-20 w-full flex-row items-center justify-around border-b border-gray-300 bg-white ">
         <div>
           <IoReorderThreeOutline className="text-2xl text-gray-600 " />
@@ -29,8 +29,8 @@ const HomePage = () => {
           </div>
         </div>
       </header>
-      <section className="grid h-full w-full grid-cols-12 place-items-center">
-        <main className="col-span-8 h-full w-full border-r border-gray-300 px-24 ">
+      <section className="grid grid-cols-12 ">
+        <main className="col-span-8 border-r border-gray-300 px-24 ">
           <div className="flex w-full flex-col space-y-4 py-10 ">
             <div className="flex w-full items-center space-x-4 ">
               <label
@@ -62,7 +62,6 @@ const HomePage = () => {
                 </div>
               </div>
             </div>
-
             <div className="flex w-full items-center justify-between border-b border-gray-300 pb-8 ">
               <div>Articles</div>
               <div>
@@ -75,7 +74,6 @@ const HomePage = () => {
               </div>
             </div>
           </div>
-
           <div className="flex w-full flex-col justify-center space-y-8 ">
             {Array.from({ length: 5 }).map((_, i) => (
               <div
@@ -131,8 +129,56 @@ const HomePage = () => {
             ))}
           </div>
         </main>
-        <aside className="col-span-4 h-full w-full ">
-          this is used for side bar
+
+        <aside className="sticky top-20 col-span-4 flex flex-col space-y-4 p-6 ">
+          <div>
+            <h3 className="my-6 text-lg font-semibold ">
+              People you might be interested
+            </h3>
+            <div className="flex flex-col space-y-4 ">
+              {Array.from({ length: 4 }).map((_, i) => (
+                <div key={i} className="flex flex-row items-center space-x-5  ">
+                  <div className="h-10 w-10 flex-none rounded-full bg-gray-400 "></div>
+                  <div>
+                    <div className="font-bold text-gray-900 ">Lorem Ipsum</div>
+                    <div className="text-xs">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Molestiae ipsum deleniti voluptas dolorum maxime!
+                    </div>
+                  </div>
+                  <div>
+                    <button className="flex items-center space-x-3 rounded border border-gray-400/50 px-4 py-2 text-gray-600 transition hover:border-gray-900 hover:text-gray-900  ">
+                      <div>Follow</div>
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="sticky top-20">
+            <h3 className="my-6 text-lg font-semibold ">Me reading list</h3>
+            <div className="flex flex-col space-y-4 ">
+              {Array.from({ length: 4 }).map((_, i) => (
+                <div key={i} className="group flex items-center space-x-6 ">
+                  <div className="aspect-square w-2/5 rounded-xl bg-gray-300 "></div>
+                  <div className="flex w-3/5 flex-col space-y-2 ">
+                    <div className="text-lg font-semibold decoration-indigo-600 group-hover:underline ">
+                      Lorem ipsum dolor sit amet consectetur.
+                    </div>
+                    <div>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Facilis, officia!
+                    </div>
+                    <div className="flex items-center space-x-4 ">
+                      <div className="h-8 w-8 rounded-full bg-gray-300 "></div>
+                      <div>Zibi Szałkowski &#x2022;</div>
+                      <div>22 Dec. 2022</div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </aside>
       </section>
     </div>
